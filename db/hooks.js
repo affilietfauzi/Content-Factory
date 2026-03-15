6ff0e7f0fcd433dabc5d68a1faf05a4a85a6ffce
affilietfauzi <affilietfauzi@gmail.com>
2026-03-15 20:18:03 +0700

    Create hooks.js

---
+++ db/hooks.js
@@ -0,0 +1,9 @@
+const hooks=[
+
+"Wait until you see this influencer lifestyle",
+"This luxury moment looks unreal",
+"POV: living the luxury influencer life",
+"You won't believe this fashion scene",
+"This cinematic influencer video looks like a movie"
+
+]
