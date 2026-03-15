00e5a2ac8a718ed3af52906548f65506eea75e2a
affilietfauzi <affilietfauzi@gmail.com>
2026-03-15 20:14:58 +0700

    Create locations.js

---
+++ db/locations.js
@@ -0,0 +1,10 @@
+const locations=[
+
+"luxury penthouse bedroom",
+"glass balcony overlooking city skyline",
+"luxury hotel rooftop",
+"Dubai marina skyline",
+"Beverly Hills luxury street",
+"Monaco harbor yacht view"
+
+]
